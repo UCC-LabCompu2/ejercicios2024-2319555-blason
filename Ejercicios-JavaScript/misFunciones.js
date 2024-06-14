b9791bd9eeb981805265d8aa7b2592cb3cbf7bb4
2319555-blason <163922621+2319555-blason@users.noreply.github.com>
2024-06-14 16:01:01 -0300

    Se realizo ejercicio de operaciones matematicas

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -61,13 +61,6 @@ function convertirGR(id, valor){
     }
 }
 
-
-
-
-/*
-*Permite sumar, restar, dividir y multiplicar
-*
- */
 /**
  * Permite mostrar u ocultar cierta información que contiene el div
  * method mostrar_ocultar
@@ -79,4 +72,45 @@ function mostrar_ocultar(valorMO){
     }else if(valorMO=="val_ocultar"){
         document.getElementById("divMO").style.display='none';
     }
+}
+
+/**
+*Permite sumar dos numeros.
+ */
+function calcularSuma(){
+    let num1, num2;
+
+    num1=Number(document.getElementsByName("sum_num1")[0].value);
+    num2=document.getElementsByName("sum_num2")[0].value;
+    document.getElementsByName("sum_total")[0].value= num1 + Number(num2);
+}
+/**
+ *Permite restar dos numeros.
+ */
+function calcularResta(){
+    let num1, num2;
+
+    num1=Number(document.getElementsByName("res_num1")[0].value);
+    num2=document.getElementsByName("res_num2")[0].value;
+    document.getElementsByName("res_total")[0].value= num1 - Number(num2);
+}
+/**
+ *Permite multiplicar dos numeros.
+ */
+function calcularMulti(){
+    let num1, num2;
+
+    num1=Number(document.getElementsByName("mul_num1")[0].value);
+    num2=document.getElementsByName("mul_num2")[0].value;
+    document.getElementsByName("mul_total")[0].value= num1 * Number(num2);
+}
+/**
+ *Permite dividir dos numeros.
+ */
+function calcularDiv(){
+    let num1, num2;
+
+    num1=Number(document.getElementsByName("div_num1")[0].value);
+    num2=document.getElementsByName("div_num2")[0].value;
+    document.getElementsByName("div_total")[0].value= num1 / Number(num2);
 }
