6eb1902e60d1c2ce4e649607d29d770de6199490
2319555-blason <163922621+2319555-blason@users.noreply.github.com>
2024-06-19 10:23:40 -0300

    Se agregaron ejercicios realizados en clases

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -134,25 +134,59 @@ function calcularDiv(){
     document.getElementsByName("div_total")[0].innerHTML= num1 / Number(num2);
 }
 /**
- *Permite cargar .
+ *Permite pasar valores de primeraweb a segundaweb.
  * @method cargarWeb
+ * @method cargarResultado
  */
 function cargarWeb(){
-var cant, unidad, urlcomp;
+var cant, unidad, urlComp;
 
     cant = document.getElementById("distancia").value;
     unidad = document.getElementsByName("unidades")[0].value;
 
-    urlcomp = "segundaWeb.html#" + cant + "#" + unidad;
-    window.open(urlcomp);
+    urlComp = "segundaWeb.html#" + cant + "#" + unidad;
+    window.open(urlComp);
     }
 
 function cargarResultado(){
     var urlComp, can, un;
 
     urlcomp = window.location.href.split("#")[5];
+    console.log(urlComp);
     can = urlComp.split("#")[1];
     un = urlComp.split("#")[2];
 
     document.getElementById("dist").value = can + " " + un;
 }
+
+function guargarLocalStorage(){
+    let distancia, unidad;
+    distancia = document.getElementById("distacia").value;
+    unidad = document.getElementsByName("unidad")[0].value;
+    localStorage.setItem("distancials", distancia);
+    localStorage.setItem("unidadesls", unidad);
+    window.open('2_web.html');
+}
+
+function cargarLocalStorage(){
+    let cant, un;
+    cant = localStorage.getItem("distacials");
+    un = localStorage.getItem("unidadesls");
+
+    document.getElementsByName("dist").value = cant + " " + un;
+
+}
+
+function dibujarCirCuad(){
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    var xMax = canvas.width;
+    var yMax = canvas.height;
+    var margen = 5;
+    ctx.fillStyle = "#333899";
+   ctx.fillRect(0,yMax-40-margen,40,40);
+
+   ctx.arc(xMax/2,yMax/2,20,0,2*Math.PI);
+   ctx.stroke();
+}
+
